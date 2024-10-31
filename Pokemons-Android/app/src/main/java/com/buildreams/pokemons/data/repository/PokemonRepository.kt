fa8dfc2ddd5138a80b94861d4e157585ecfaffce
Camilo Andrés Rodríguez Garzón <cam_rod@hotmail.com>
2024-10-31 23:34:27 +0400

    add Pokemon repository

---
+++ Pokemons-Android/app/src/main/java/com/buildreams/pokemons/data/repository/PokemonRepository.kt
@@ -0,0 +1,14 @@
+package com.buildreams.pokemons.data.repository
+
+import com.buildreams.pokemons.data.datasource.PokemonRemote
+import com.buildreams.pokemons.data.model.Pokemon
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import javax.inject.Inject
+
+class PokemonRepository @Inject constructor(private val pokemonApi: PokemonRemote) {
+
+    suspend fun getPokemon(id: Int): Flow<Pokemon> = flow {
+        emit(pokemonApi.getPokemon(id))
+    }
+}
